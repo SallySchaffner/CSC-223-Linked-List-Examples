97a037bf758dd8dfcce5cc3098dd738c6f6d7058
Sally Schaffner <schaffners@vpcc.edu>
2024-12-30 16:56:21 -0500

    Added tail pointer and count to linked list class

--- CSC 223 Linked List Examples.cpp
+++ CSC 223 Linked List Examples.cpp
@@ -1,17 +1,71 @@
 // CSC 223 Linked List Examples.cpp : Demonstrate a singly linked list
 #include "SinglyLinkedList.h"
 
+void BuildListForward();
+void BuildListBackward();
+
 
 int main() {
-    SinglyLinkedList list;
-    list.insertAtBeginning(10);
-    list.insertAtEnd(20);
-    list.insertAtEnd(30);
-    list.display(); // Output: 10 -> 20 -> 30 -> nullptr
-
-    list.deleteValue(20);
-    list.display(); // Output: 10 -> 30 -> nullptr
+    
+    BuildListForward();
+    BuildListBackward();
 
     return 0;
+}
+
+void BuildListForward()
+{
+    SinglyLinkedList list;
+    std::cout << "Build a forward list" << std::endl;
+    for (int i = 10; i < 60; i += 10)
+    {
+        list.insertAtEnd(i);
+    }
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the first node" << std::endl;
+    list.deleteValue(10);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the last node" << std::endl;
+    list.deleteValue(50);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the middle node" << std::endl;
+    list.deleteValue(30);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+}
+
+void BuildListBackward()
+{
+    SinglyLinkedList list;
+    std::cout << "Build a backward list" << std::endl;
+    for (int i = 10; i < 60; i += 10)
+    {
+        list.insertAtBeginning(i);
+    }
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the first node" << std::endl;
+    list.deleteValue(50);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the last node" << std::endl;
+    list.deleteValue(10);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+    std::cout << "Delete the middle node" << std::endl;
+    list.deleteValue(30);
+    std::cout << list.getCount() << std::endl;
+    list.display();
+    std::cout << std::endl;
+
 }
 
